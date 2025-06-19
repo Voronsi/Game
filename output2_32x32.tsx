47ece253fdde3063eebfa16cd8978eaf1eff793d
Sergey <167085988+Voronsi@users.noreply.github.com>
2025-06-20 05:11:05 +0700

    Delete output2_32x32.tsx

--- output2_32x32.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="output2_32x32" tilewidth="32" tileheight="32" tilecount="256" columns="8">
- <image source="output2_32x32.png" width="256" height="1024"/>
-</tileset>
